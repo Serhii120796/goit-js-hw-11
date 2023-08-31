67bc96441a5678519ff00e5e887186b9727d1f31
Serhii120796 <sfedyura@ukr.net>
2023-08-31 22:49:25 +0300

    added notiflix

--- src/index.js
+++ src/index.js
@@ -1,24 +1,62 @@
 import { fetchImages } from './js/pixabay-api.js';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+Notify.init({
+  position: 'center-center',
+  backOverlay: true,
+  clickToClose: true,
+});
 
 const form = document.querySelector('.search-form');
 const list = document.querySelector('.gallery');
-form.addEventListener('submit', handlerSubmit);
+const button = document.querySelector('.load-more');
 
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  list.innerHTML = '';
+  addImages(evt);
+});
+
+let page = 1;
+
+button.addEventListener('click', () => {
+  page += 1;
+  addImages();
+  
+});
 const { searchQuery: input } = form.elements;
 
-function handlerSubmit(evt) {
-  evt.preventDefault();
+function addImages() {
+  
 
-  fetchImages(input.value)
-      .then(response => {
-          list.insertAdjacentHTML('beforeend', createMarcup(response.data));
-          console.log(response.data);
-      })
+  fetchImages(input.value, page)
+    .then(response => {
+      if (response.data.hits.length) {
+        list.insertAdjacentHTML('beforeend', createMarcup(response.data));
+        button.classList.remove('hidden');
+        
+      } else {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+
+      console.log(response.data);
+    })
     .catch(error => console.log(error));
 }
 
 function createMarcup({ hits }) {
-    return hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
+  return hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
         `<div class="photo-card">
   <img src="${webformatURL}" alt="${tags}" class="gallery-img" loading="lazy" />
   <div class="info">
@@ -35,5 +73,7 @@ function createMarcup({ hits }) {
       <b>Downloads</b> ${downloads}
     </p>
   </div>
-</div>`).join('');
+</div>`
+    )
+    .join('');
 }
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = '38986631-ae11b42db00bd05f0f2571500';
 const BASE_URL = "https://pixabay.com/api/";
 
-function fetchImages(searchData) {
+function fetchImages(searchData, page) {
  
   const params = new URLSearchParams({
     key: API_KEY,
@@ -11,6 +11,8 @@ function fetchImages(searchData) {
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
+    per_page: 40,
+    page: page,
   });
 
     return axios.get(`${BASE_URL}?${params}`);
